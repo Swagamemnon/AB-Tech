6e196ab0ec174a7754f7ca2dfb643be17b47a048
Noah Decker <noahdecker724@gmail.com>
2024-02-02 14:32:01 -0500

    Add files via upload

---
+++ my_guest_list.py
@@ -0,0 +1,6 @@
+# list of dinner guests
+guest_list = ['kanye west', 'barack obama', 'leonardo da Vinci']
+# now invite them to dinner!
+print(f"Dear {guest_list[0].title()}, I would like to cordially invite you to a VIP dinner.")
+print(f"Dear {guest_list[1].title()}, I would like to cordially invite you to a VIP dinner.")
+print(f"Dear {guest_list[2].title()}, I would like to cordially invite you to a VIP dinner.")
