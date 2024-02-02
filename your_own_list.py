9e5ec659585727bf570a2e42cee8214f9a4858b5
Noah Decker <noahdecker724@gmail.com>
2024-02-02 14:26:16 -0500

    Add files via upload

---
+++ your_own_list.py
@@ -0,0 +1,9 @@
+# create a list of your favorite modes of transportation
+fancy_cars = ['rolls-royce phantom', 'lambourghini aventador', 'bentley continental',
+              'aston martin DBS', 'porsche 911']
+# print statements about list items
+print(f"I hope I can drive a {fancy_cars[0].title()} someday!")
+print(f"I hope I can drive a {fancy_cars[1].title()} someday!")
+print(f"I hope I can drive a {fancy_cars[2].title()} someday!")
+print(f"I hope I can drive a {fancy_cars[3].title()} someday!")
+print(f"I hope I can drive a {fancy_cars[4].title()} someday!")
